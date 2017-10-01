636602d84893a396e3c61860b38764e087e221eb
AlisherB <alisher_b_95@mail.ru>
2017-10-01 18:50:57 +0600

    HW_WinForms_FirstHW

---
+++ FirstHomework/Task1/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Task1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resumeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // resumeButton
+            // 
+            this.resumeButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.resumeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.resumeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.resumeButton.Location = new System.Drawing.Point(19, 13);
+            this.resumeButton.Name = "resumeButton";
+            this.resumeButton.Size = new System.Drawing.Size(91, 33);
+            this.resumeButton.TabIndex = 0;
+            this.resumeButton.Text = "Резюме";
+            this.resumeButton.UseVisualStyleBackColor = true;
+            this.resumeButton.Click += new System.EventHandler(this.ResumeButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(128, 56);
+            this.Controls.Add(this.resumeButton);
+            this.MaximumSize = new System.Drawing.Size(144, 95);
+            this.MinimumSize = new System.Drawing.Size(144, 95);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button resumeButton;
+    }
+}
+
---
+++ FirstHomework/Task1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Task1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ResumeButton_Click(object sender, EventArgs e)
+        {
+            string surname = "Бекмухамбет";
+            string name = "Алишер";
+            string middleName = "Айболатулы";
+            string birthday = "08.07.1995";
+            string education = "Академии Шаг";
+            MessageBox.Show(surname, "Фамилия");
+            MessageBox.Show(name, "Имя");
+            MessageBox.Show(middleName, "Отчество");
+            MessageBox.Show(birthday, "Дата рождения");
+            int length = (surname.Length + name.Length + middleName.Length + birthday.Length + education.Length) / 5;
+            MessageBox.Show(education, length.ToString());
+        }
+    }
+}
